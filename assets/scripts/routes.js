76ed89c4e26f847480e18463fc2ccdb5266ad491
John Peart <code@johnpe.art>
2022-04-03 20:31:49 +0000

    Various changes

--- assets/scripts/routes.js
+++ assets/scripts/routes.js
@@ -75,3 +75,50 @@ const locationHandler = async () => {
 window.addEventListener("hashchange", locationHandler);
 // call the urlLocationHandler to load the page
 locationHandler();
+
+//////////////////////////////
+// Other UI related scripts //
+//////////////////////////////
+
+var appWindow = document.documentElement;
+
+var buttonSidebarToggle = document.getElementById("toggle-menu");
+buttonSidebarToggle.addEventListener("click", toggleSidebar);
+
+var fullscreenToggle = document.getElementById("toggle-fullscreen");
+fullscreenToggle.addEventListener("click", openFullScreen, appWindow);
+
+function toggleSidebar() {
+
+  let app = document.getElementById("app")
+	if (app.dataset.sidebar == "show") {
+		app.dataset.sidebar = "hide";
+    // openFullscreen(app);
+	} else {
+		app.dataset.sidebar = "show";
+    // closeFullscreen(app);
+	}
+
+}
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+	 elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+	 elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+	 elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen(elem) {
+  if (document.exitFullscreen) {
+	 document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+	 document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+	 document.msExitFullscreen();
+  }
+}
